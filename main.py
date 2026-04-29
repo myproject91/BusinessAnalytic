44c5a3af26bf0a038e415b7a74f20da9d43fac92
myproject91 <koqcantik@gmail.com>
2026-04-29 14:45:39 +0700

    add CORS middleware

--- main.py
+++ main.py
@@ -1,10 +1,18 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from routers.analyze import router
 
 app = FastAPI(title='AI Business Intelligence API')
 
-app.include_router(router, prefix='/api/v1')
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
+app.include_router(router, prefix='/api/v1')
 
 @app.get('/')
 def root():
